f40b48eb9c9b71718e774ed3a2e91ef8a8c094e5
Nicolas Voges <nicolas.voges@hotmail.com>
2025-08-29 11:46:11 +0200

    Feat: Add user profile model.

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class UserType(models.TextChoices):
+    customer = 'customer', 'Customer'
+    business = 'business', 'Business'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    file = models.ImageField(upload_to='user_images/')
+    location = models.CharField(max_length=30)
+    tel = PhoneNumberField()
+    description = models.CharField(max_length=255)
+    working_hours = models.CharField(max_length=20)
+    type = models.CharField(max_length=20, choices=UserType.choices, default=UserType.customer)
+    email = models.EmailField()
+    created_at = models.DateTimeField()
--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'phonenumber_field',
     'coderr_app',
     'auth_app',
 ]
@@ -120,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
