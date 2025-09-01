abf0024ab884eb0d363c84c77e645fa6889d2d01
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-01 23:21:52 +0200

    Test: Adjust registration test.

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -1,21 +1,30 @@
+from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
-class RegistrationTests(APITestCase):
+class RegistrationTests(APITestCase):       
+
     def test_registration_post(self):
         url = reverse('registration')
         data = {
-            "username": "exampleUsername",
-            "email": "example@mail.de",
+            "username": "TestUser",
+            "email": "test@user.de",
             "password": "examplePassword",
             "repeated_password": "examplePassword",
             "type": "customer"
         }
+
         response = self.client.post(url, data, format='json')
-        expected_data = RegistrationSerializer(data).data
-        self.assertDictEqual(response.data, expected_data)
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('token', response.data)
+        self.assertIn('user_id', response.data)
+        self.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])
+
         response = self.client.post(url, data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
