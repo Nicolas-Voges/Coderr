6fedcf6fff4b4b5c5dcc1a4628160fd5994ad2e3
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-03 04:17:34 +0200

    Test: Add profile detail tests.

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
+from auth_app.models import Profile
 
 class RegistrationTests(APITestCase):
 
@@ -86,3 +88,70 @@ class LoginTests(APITestCase):
         response = self.client.post(self.url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileTests(APITestCase):
+
+    def setUp(self):
+        self.url_detail = reverse('profile-detail')
+        self.url_business = reverse('profile_business-list')
+        self.url_customer = reverse('profile_customer-list')
+        self.username = "exampleUsername"
+        self.password = "examplePassword"
+        self.email = "example@mail.de"
+        self.first_name = 'Max'
+        self.last_name = ''
+        self.image_name = "test_image.jpg"
+        self.image = SimpleUploadedFile(
+            name=self.image_name,
+            content=b"fake image content",
+            content_type="image/jpeg"
+        )
+        self.user = User.objects.create_user(username=self.username,
+                                             password=self.password,
+                                             email=self.email,
+                                             first_name=self.first_name,
+                                             last_name=self.last_name)
+        self.token = Token.objects.create(user=self.user)
+        self.profile = Profile.objects.create(
+            user=self.user.id,
+            username=self.username,
+            first_name=self.first_name,
+            last_name=self.last_name,
+            file=self.image,
+            location="Berlin",
+            tel="123456789",
+            description="Business description",
+            working_hours="9-17",
+            type="business",
+            email=self.email,
+        )
+
+
+    def test_get_detail_success(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        response = self.client.get(self.url_detail)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['user'], self.user.id)
+        self.assertEqual(response.data['username'], self.username)
+        self.assertIsInstance(response.data['first_name'], str, msg='First name is not a string!')
+        self.assertIsInstance(response.data['last_name'], str, msg='Last name is not a string!')
+        self.assertEqual(response.data['first_name'], self.first_name)
+        self.assertEqual(response.data['last_name'], self.last_name)
+        self.assertTrue(response.data['file'].name.endswith(self.image_name))
+        self.assertIsInstance(response.data['location'], str, msg='Location name is not a string!')
+        self.assertIn('tel', response.data)
+        self.assertIsInstance(response.data['description'], str, msg='Description is not a string!')
+        self.assertIsInstance(response.data['working_hours'], str, msg='Working_hours is not a string!')
+        self.assertEqual(response.data['type'], self.profile.type)
+        self.assertEqual(response.data['email'], self.email)
+        self.assertEqual(response.data['email'], self.email)
+        self.assertIsInstance(response.data['create_at'], str, msg='Create_at is not a string!')
+
+
+    def test_get_detail_fails_not_authorized(self):
+        response = self.client.get(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
