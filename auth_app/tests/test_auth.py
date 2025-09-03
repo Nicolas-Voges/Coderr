9ad589cfa03d85706ba7fb5d14f2a030e713096d
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-04 01:25:52 +0200

    Test: Add profile detail tests.

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -106,6 +106,16 @@ class ProfileTests(APITestCase):
             content=b"fake image content",
             content_type="image/jpeg"
         )
+        self.patch_data = {
+            "first_name": "Max",
+            "last_name": "Mustermann",
+            "location": "Berlin",
+            "tel": "987654321",
+            "description": "Updated business description",
+            "working_hours": "10-18",
+            "email": "new_email@business.de",
+            "file": self.image
+        }
         self.user = User.objects.create_user(username=self.username,
                                              password=self.password,
                                              email=self.email,
@@ -167,37 +177,46 @@ class ProfileTests(APITestCase):
 
 
     def test_patch_detail_success(self):
-        image_name = "test_image.jpg"
-        image = SimpleUploadedFile(
-            name=image_name,
-            content=b"fake image content",
-            content_type="image/jpeg"
-        )
-        data = {
-            "first_name": "Max",
-            "last_name": "Mustermann",
-            "location": "Berlin",
-            "tel": "987654321",
-            "description": "Updated business description",
-            "working_hours": "10-18",
-            "email": "new_email@business.de",
-            "file": image
-        }
+        
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-        response = self.client.patch(self.url_detail, data, format='json')
+        response = self.client.patch(self.url_detail, self.patch_data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['user'], self.user.id)
         self.assertEqual(response.data['username'], self.username)
-        self.assertEqual(response.data['first_name'], data['first_name'])
-        self.assertEqual(response.data['last_name'], data['last_name'])
-        self.assertEqual(response.data['location'], data['location'])
-        self.assertEqual(response.data['tel'], data['tel'])
-        self.assertEqual(response.data['description'], data['description'])
-        self.assertEqual(response.data['working_hours'], data['working_hours'])
-        self.assertEqual(response.data['email'], data['email'])
+        self.assertEqual(response.data['first_name'], self.patch_data['first_name'])
+        self.assertEqual(response.data['last_name'], self.patch_data['last_name'])
+        self.assertEqual(response.data['location'], self.patch_data['location'])
+        self.assertEqual(response.data['tel'], self.patch_data['tel'])
+        self.assertEqual(response.data['description'], self.patch_data['description'])
+        self.assertEqual(response.data['working_hours'], self.patch_data['working_hours'])
+        self.assertEqual(response.data['email'], self.patch_data['email'])
         self.assertEqual(response.data['type'], self.profile.type)
-        self.assertTrue(response.data['file'].name.endswith(image_name))
+        self.assertTrue(response.data['file'].name.endswith(self.image_name))
         self.assertIsInstance(response.data['created_at'], str, msg='First name is not a string!')
 
+
+    def test_patch_detail_fails_not_authorized(self):
+        response = self.client.patch(self.url_detail, self.patch_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_patch_detail_fails_not_authorized(self):
+        second_user = User.objects.create_user(username='Test2', password='Test12§$')
+        second_token = Token.objects.create(user=second_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + second_token.key)
+
+        response = self.client.patch(self.url_detail, self.patch_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    
+    def test_patch_detail_fails_user_not_exists(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        url = reverse('profile-detail', kwargs={'pk': 9999})
+
+        response = self.client.patch(url, self.patch_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
