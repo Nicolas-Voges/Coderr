91d62b331036885ea2e442e36af3b0db7c6c54e3
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-05 18:16:12 +0200

    Feat: Add profile retrieve URL and seasson authentication

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegistrationView, LoginView
+from .views import RegistrationView, LoginView, ProfileUpdateRetriveView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name="registration"),
     path('login/', LoginView.as_view(), name="login"),
+    path('profile/<int:pk>/', ProfileUpdateRetriveView.as_view(), name="profile-detail"),
 ]
