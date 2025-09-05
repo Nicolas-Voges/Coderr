3d7c3a51b8fb7aa1c11b337e5227aa4077b14887
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-05 21:21:02 +0200

    Feat: URLs added for lists with customer and business profiles.

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegistrationView, LoginView, ProfileUpdateRetriveView
+from .views import RegistrationView, LoginView, ProfileUpdateRetriveView, ProfileListView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name="registration"),
     path('login/', LoginView.as_view(), name="login"),
     path('profile/<int:pk>/', ProfileUpdateRetriveView.as_view(), name="profile-detail"),
+    path('profiles/business/', ProfileListView.as_view(), name="profile_business-list"),
+    path('profiles/customer/', ProfileListView.as_view(), name="profile_customer-list"),
 ]
