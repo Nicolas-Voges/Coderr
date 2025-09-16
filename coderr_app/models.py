970eb45cb5ff29425137fdafa9e05f517b367474
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-16 23:06:09 +0200

    Feat: Add detail model.

--- coderr_app/models.py
+++ coderr_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class DetailType(models.TextChoices):
+    basic = 'basic', 'Basic'
+    standard = 'standard', 'Standard'
+    premium = 'premium', 'Premium'
+
+    
+class Detail(models.Model):
+    title = models.CharField(max_length=50)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=20, choices=DetailType.choices, default=DetailType.basic)
