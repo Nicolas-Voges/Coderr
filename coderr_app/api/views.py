8f354047c7240f63898bb936725b6b253f89ae7e
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-21 00:40:35 +0200

    Feat: Get permissions for offer view set.

--- coderr_app/api/views.py
+++ coderr_app/api/views.py
@@ -2,11 +2,22 @@ from rest_framework import viewsets, status, generics, mixins
 from coderr_app.models import Offer, Detail
 from rest_framework.permissions import IsAuthenticated
 from .serializers import OfferSerializer, DetailSerializer
+from .permissions import IsTypeBusiness, IsOwner
 
 class OfferViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
     serializer_class = OfferSerializer
     queryset = Offer.objects.all()
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = []
+        if self.action == 'create':
+            permission_classes = [IsAuthenticated, IsTypeBusiness]
+        if self.action == 'retrieve':
+            permission_classes = [IsAuthenticated]
+        if self.action == 'update' or self.action == 'destroy':
+            permission_classes = [IsAuthenticated, IsOwner]
+        return [permission() for permission in permission_classes]
 
 class DetailRetrieveView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
