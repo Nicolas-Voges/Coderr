9781ff6488952917024e1f208c36848a4939e7ff
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-21 01:26:22 +0200

    Feat: Permission added for whether the user is owner.

--- coderr_app/api/permissions.py
+++ coderr_app/api/permissions.py
@@ -1,9 +1,15 @@
 from rest_framework.permissions import BasePermission
 
 class IsTypeBusiness(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.user
+    def has_permission(self, request, view):
+        return request.user.type == 'business'
+
 
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated and request.user.is_superuser
+    
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.user
