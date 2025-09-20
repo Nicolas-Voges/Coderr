a7b776f2fd67d499c2c771a316c3d159adc3a281
Nicolas Voges <nicolas.voges@hotmail.com>
2025-09-21 01:15:27 +0200

    Feat: Permission added for whether the user is a super user.

--- coderr_app/api/permissions.py
+++ coderr_app/api/permissions.py
@@ -3,3 +3,7 @@ from rest_framework.permissions import BasePermission
 class IsTypeBusiness(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user
+
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_superuser
