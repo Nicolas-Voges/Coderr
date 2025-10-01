35931903509a6afbaaaed329dccfbdbd77907ae4
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-01 17:56:46 +0200

    Feat: Add filter, order and limit functions for queryset in OfferViewSet class.

--- coderr_app/api/views.py
+++ coderr_app/api/views.py
@@ -21,6 +21,35 @@ class OfferViewSet(viewsets.ModelViewSet):
             permission_classes = [IsSuperUser]
 
         return [permission() for permission in permission_classes]
+    
+
+    def get_queryset(self):
+        queryset = Offer.objects.all()
+        page_size_param = self.request.query_params.get('page_size',12)
+
+        creator_id_param = self.request.query_params.get('creator_id',None)
+        if creator_id_param is not None:
+            queryset = queryset.filter(user_id=creator_id_param)
+
+        min_price_param = self.request.query_params.get('min_price',None)
+        if min_price_param is not None:
+            queryset = queryset.filter(min_price__lte=min_price_param)
+
+        max_delivery_time_param = self.request.query_params.get('max_delivery_time',None)
+        if max_delivery_time_param is not None:
+            queryset = queryset.filter(max_delivery_time_param__lte=max_delivery_time_param)
+
+        ordering_param = self.request.query_params.get('ordering',None)
+        if ordering_param is not None:
+            queryset = queryset.order_by(ordering_param)
+
+        search_param = self.request.query_params.get('search',None) 
+        if search_param is not None:
+            queryset = queryset.filter(title__icontains=search_param)
+            queryset = queryset.filter(description__icontains=search_param)
+        queryset = queryset.all()[:page_size_param]
+        return queryset
+    
 
 class DetailRetrieveView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
