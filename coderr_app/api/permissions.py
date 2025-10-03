33761ed0e1222405b53753654929252a1c320d55
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-03 12:00:52 +0200

    Feat: Added permissions for OrderViewSet.

--- coderr_app/api/permissions.py
+++ coderr_app/api/permissions.py
@@ -5,6 +5,12 @@ class IsTypeBusiness(BasePermission):
     def has_permission(self, request, view):
         user_type = Profile.objects.get(user=request.user).type
         return user_type == 'business'
+    
+
+class IsTypeCustomer(BasePermission):
+    def has_permission(self, request, view):
+        user_type = Profile.objects.get(user=request.user).type
+        return user_type == 'customer'
 
 
 class IsSuperUser(BasePermission):
@@ -12,6 +18,11 @@ class IsSuperUser(BasePermission):
         return request.user and request.user.is_authenticated and request.user.is_superuser
     
 
+class IsStaffUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_staff
+    
+
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.user
+        return request.user == obj.user or request.user == obj.business_user
